998e36e7869e8510931caed244d15f7d1bf7cb99
alexciotor <ciotor.alex@yahoo.com>
2021-02-12 02:34:54 +0200

    refuse btn added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+ import {GiConfirmed} from 'react-icons/gi'
  import data from './data'
 import List from './list'
 import {useState} from 'react'
 function App() {
  const [people, setPeople]= useState(data)
+ const attendedPpl=(id)=>{
+setPeople(people.filter(item=>item.id !==id))
+ }
   return (
     <div className='main-container'  >
   <h4>{people.length} Birthdays today</h4>
-<List people={people}/>
+<List attendedPpl={attendedPpl} people={people}/>
 <button onClick={()=>{
       setPeople([])
   }} className="btn">Clear all</button>
--- src/list.js
+++ src/list.js
@@ -1,13 +1,21 @@
-const List = ({people})=>{
-    
+ import {GiConfirmed} from 'react-icons/gi'
+ import React, {useState} from 'react'
+const List = ({attendedPpl,people})=>{
+const [attend, setAttend] = useState(false)
+
 return <main>
  {people.map((item,index)=>{
      const {id,name,age,image} =item;
      return <article key={index} className='article' >
      <img className='image' src={image} alt={name}/>
-     <header>
+     <header className='header' >
      <h4>{name}</h4>
      <p>{age} years</p>
+     <div className="buttons">
+         <button onClick={()=>{
+        attendedPpl(id)
+    }} className="btns">refuse</button>
+     </div>
      </header>
      </article>
     
