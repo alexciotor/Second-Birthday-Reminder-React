7e0257695f18293006069dfd0428389204c150e1
alexciotor <ciotor.alex@yahoo.com>
2021-02-12 06:53:03 +0200

    another btn added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
- import {GiConfirmed} from 'react-icons/gi'
+
  import data from './data'
 import List from './list'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 function App() {
+  const [btn, setBtn]= useState(false)
  const [people, setPeople]= useState(data)
  const attendedPpl=(id)=>{
 setPeople(people.filter(item=>item.id !==id))
  }
+
+ useEffect(()=>{
+    if(people.length ===0){
+   setBtn(true)
+   
+ }
+ else{
+   setBtn(false)
+ }
+ },[people.length])
   return (
     <div className='main-container'  >
   <h4>{people.length} Birthdays today</h4>
@@ -14,6 +25,11 @@ setPeople(people.filter(item=>item.id !==id))
 <button onClick={()=>{
       setPeople([])
   }} className="btn">Clear all</button>
+  {btn && <button onClick={()=>{
+      setPeople([])
+  }} className="btn" onClick={()=>{
+    setPeople(data)
+  }}>think it through</button>}
     </div>
   );
 }
