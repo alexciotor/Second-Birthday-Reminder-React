bbca4d402f7bccf38df2e8f21d60f2f37fef1a62
alexciotor <ciotor.alex@yahoo.com>
2021-02-13 00:00:50 +0200

    iteration moved from list to app js

--- src/list.js
+++ src/list.js
@@ -1,25 +1,30 @@
  import {GiConfirmed} from 'react-icons/gi'
  import React, {useState} from 'react'
-const List = ({attendedPpl,people})=>{
+const List = ({attendedPpl,item})=>{
 const [attend, setAttend] = useState(false)
-
-return <main>
- {people.map((item,index)=>{
+const[refuse,setRefuse] = useState(true)
      const {id,name,age,image} =item;
-     return <article key={index} className='article' >
+return <main>
+ 
+
+       <article key={id} className='article' >
      <img className='image' src={image} alt={name}/>
      <header className='header' >
      <h4>{name}</h4>
      <p>{age} years</p>
      <div className="buttons">
-         <button onClick={()=>{
+     <button onClick={()=>{
+         setAttend(!attend)
+         setRefuse(!refuse)
+     }} className="btns">{attend? <span className='span-attend' > <GiConfirmed/></span> : 'Attend'}</button>
+      { refuse &&   <button onClick={()=>{
         attendedPpl(id)
-    }} className="btns">refuse</button>
+    }} className="btns">refuse</button>}
      </div>
      </header>
      </article>
     
- })}
+
   
 </main>
 }
